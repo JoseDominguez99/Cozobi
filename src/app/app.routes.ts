f291b72daf3cfe28ff41462c6d9b02c85d65b647
Jose <utti222001@utvco.edu.mx>
2025-03-18 18:27:20 -0600

    sin tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardCozobiComponent } from './dashboard/dashboardCozobi/dashboardCozobi.component';
+import { SelectionComponent } from './dashboard/pages/selection/selection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardCozobiComponent,
+        children: [
+            {
+                path: 'selection',
+                component: SelectionComponent
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'app',
+        pathMatch: 'full'
+    }
+];
