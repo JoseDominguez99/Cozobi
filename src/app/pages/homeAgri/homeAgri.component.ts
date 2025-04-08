4347d6d893672097c8f2963add4ecb05d6b54a15
F1oxyz <angelafloxz@gmail.com>
2025-04-08 12:28:36 -0600

    homeAgri cambios

--- src/app/pages/homeAgri/homeAgri.component.ts
+++ src/app/pages/homeAgri/homeAgri.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MapaComponent } from '../../components/mapa/mapa.component';
 
 @Component({
   selector: 'app-home-agri',
-  imports: [HeaderComponent, MapaComponent],
+  imports: [HeaderComponent, MapaComponent, CommonModule],
   templateUrl: './homeAgri.component.html',
   styles: `
     :host {
@@ -13,4 +14,20 @@ import { MapaComponent } from '../../components/mapa/mapa.component';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeAgriComponent { }
+export class HomeAgriComponent { 
+
+  ubicacion = {
+    nombre: 'San Pablo Huixtepec, Oaxaca, México',
+    suelo: 'Suelo franco',
+    temperatura: 24,
+    humedad: 65
+  };
+
+  cultivos = [
+    { nombre: 'Maíz', familia: 'Gramineae, Zea mays' },
+    { nombre: 'Frijol', familia: 'Fabaceae, Phaseolus vulgaris' },
+    { nombre: 'Tomate', familia: 'Solanaceae, Solanum lycopersicum' }
+  ];
+
+
+}
